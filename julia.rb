29c9f29946a85742199716b695b2b0b255e90be7
Katja Miethe <katjamiethe@gmail.com>
2013-06-01 17:21:00 +0200

    Schwatz v02

--- julia.rb
+++ julia.rb
@@ -5,16 +5,25 @@ class Julia
 	def initialize
 	end
 
-	def paint
-		png = ChunkyPNG::Image.new(200, 200, ChunkyPNG::Color::WHITE)
-		png[0, 0] = ChunkyPNG::Color::BLACK
-		
+	def black
 		a = 0
+		b = 0
 		while a < 200
-  		png[a, 0] = ChunkyPNG::Color::BLACK
-  		a += 1
+			@png[a, b] = ChunkyPNG::Color::BLACK
+			while b < 200
+				@png[a, b] = ChunkyPNG::Color::BLACK
+				b += 1
+			end
+			b = 0
+			a += 1
+		end
 	end
-		png.save('julia.png')
+
+	def paint
+		@png = ChunkyPNG::Image.new(200, 200, ChunkyPNG::Color::WHITE)
+		@png[0, 0] = ChunkyPNG::Color::BLACK
+		black
+		@png.save('julia.png')
 	end
 end
 
