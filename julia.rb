0ddd25fb45edbfd110e1581e15e318fb9b91baf9
Katja Miethe <katjamiethe@gmail.com>
2013-06-01 15:23:43 +0200

    Bild Schwarz gemalt

--- julia.rb
+++ julia.rb
@@ -7,7 +7,14 @@ class Julia
 
 	def paint
 		png = ChunkyPNG::Image.new(200, 200, ChunkyPNG::Color::WHITE)
-		png.save('julia.png', :interlace => true)
+		png[0, 0] = ChunkyPNG::Color::BLACK
+		
+		a = 0
+		while a < 200
+  		png[a, 0] = ChunkyPNG::Color::BLACK
+  		a += 1
+	end
+		png.save('julia.png')
 	end
 end
 
